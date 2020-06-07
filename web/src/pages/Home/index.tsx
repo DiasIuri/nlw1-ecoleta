39524c61080b045ce49e5ecc39afa84dd099b414
Iuri Dias <iuridiasemiliano@hotmail.com>
2020-06-06 23:02:44 -0400

    Redirecionamento da home para create-point

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -9,19 +10,21 @@ const Home = () => {
     <div id="page-home">
       <div className="content">
         <header>
-          <img src={logo} alt="Ecoleta" />
+          <Link to="/">
+            <img src={logo} alt="Ecoleta" />
+          </Link>
         </header>
 
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
