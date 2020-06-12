fff6374e8d0349fb9a4e670a7101df6af2b6e0d8
Iuri Dias <iuridiasemiliano@hotmail.com>
2020-06-12 02:34:17 -0400

    Adição de upload de imagens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const itensController = new ItemsController();
 const pointsController = new PointsController();
 
 routes.get('/items', itensController.index);
 
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
